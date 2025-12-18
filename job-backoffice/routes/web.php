8778531e3bef01ff150e8d7da9244888961cb799
Abdelrahman Altamimi <tamimi6030@gmail.com>
2025-12-18 21:50:47 +0300

    fix layout navigation

--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -1,13 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/',[DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/company',[CompanyController::class, 'index'])->name('company.index');
+    Route::get('/application',[JobApplicationController::class, 'index'])->name('application.index');
+    Route::get('/category',[JobCategoryController::class, 'index'])->name('category.index');
+    Route::get('/job-vacancy',[JobVacancyController::class, 'index'])->name('job-vacancy.index');
+    Route::get('/user',[UserController::class, 'index'])->name('user.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
